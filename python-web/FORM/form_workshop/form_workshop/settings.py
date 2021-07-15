dbb7184a10049d71a647a9c54c8290ef613cefad
yosif88 <yosif.karaboychev88@gmail.com>
2021-07-15 16:02:52 +0300

    modified

--- python-web/FORM/form_workshop/form_workshop/settings.py
+++ python-web/FORM/form_workshop/form_workshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6fw50e%gt=tr*r9h3xvivhw)k5=4c#2@3+4h5((p46dvjs2z!i'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- python-web/petstagram/petstagram/settings.py
+++ python-web/petstagram/petstagram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e!75+o^s1qh7s8$^5!48-@acfycfid*6mraa_k#%l5w#hk9k!('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
