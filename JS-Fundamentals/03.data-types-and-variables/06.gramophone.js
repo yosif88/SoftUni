6a2498556625aec926e0b8ba29788003028e28d3
yosif88 <yosif.karaboychev88@gmail.com>
2023-05-19 10:51:05 +0300

    Added task

--- JS-Fundamentals/03.data-types-and-variables/06.gramophone.js
+++ JS-Fundamentals/03.data-types-and-variables/06.gramophone.js
@@ -1,1 +1,8 @@
-function gramophone()
+function gramophone(bandName, albumName, songName) {
+   
+    let rotations = ((albumName.length * bandName.length) * songName.length / 2) / 2.5;
+    console.log(`The plate was rotated ${Math.ceil(rotations)} times.`);
+
+}
+
+gramophone('Black Sabbath', 'Paranoid', 'War Pigs')
---
+++ JS-Fundamentals/03.data-types-and-variables/07.requiredReading.js
@@ -0,0 +1,9 @@
+function requiredReading(numberOfPages, pagesReadPerHour,days) {
+    let result = (numberOfPages / pagesReadPerHour)/ days;
+    console.log(result);
+
+}
+
+requiredReading(212,
+    20 ,
+    2)
