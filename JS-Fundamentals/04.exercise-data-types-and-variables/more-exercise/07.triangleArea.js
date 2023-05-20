172a468d0212eb1b3363ac5192e0de353982c355
yosif88 <yosif.karaboychev88@gmail.com>
2023-05-20 20:35:34 +0300

    Added task

---
+++ JS-Fundamentals/04.exercise-data-types-and-variables/more-exercise/07.triangleArea.js
@@ -0,0 +1,12 @@
+function triangleArea(a, b, c) {
+    // formula for the semiperimeter
+    let s = (a + b + c) / 2;
+  
+    let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    console.log(area);
+    
+}
+
+triangleArea(2,
+    3.5,
+    4)
