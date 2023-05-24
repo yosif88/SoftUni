9131c75ce469e01c0c9c6af2ffcc671e5f86b781
yosif88 <yosif.karaboychev88@gmail.com>
2023-05-24 19:47:01 +0300

    added task

---
+++ JS-Fundamentals/05.arrays/01.sumFirstAndLastArrayElements.js
@@ -0,0 +1,9 @@
+function sumFirstAndLastElements(arr){
+    let firstNum = arr[0];
+    let lastNum = arr[arr.length - 1];
+    let result = firstNum + lastNum;
+    console.log(result);
+    
+}
+
+sumFirstAndLastElements([20, 30, 40])
