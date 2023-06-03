40ec63f11463b9d3874b466d360eb9636730b04a
yosif88 <yosif.karaboychev88@gmail.com>
2023-06-03 11:51:20 +0300

    added task

---
+++ JS-Fundamentals/07.functions/02.mathPower.js
@@ -0,0 +1,11 @@
+function mathPower(number, power){
+    let result = 1;
+
+    for (let i = 0; i < power; i++){
+        result *= number;
+        
+    }
+    console.log(result);
+}
+
+mathPower(2, 8)
