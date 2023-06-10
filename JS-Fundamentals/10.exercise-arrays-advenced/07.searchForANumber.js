9e4d061446588a880d0942840d04e0a3a541b818
yosif88 <yosif.karaboychev88@gmail.com>
2023-06-11 00:05:58 +0300

    searchForANumber

---
+++ JS-Fundamentals/10.exercise-arrays-advenced/07.searchForANumber.js
@@ -0,0 +1,25 @@
+function searchForANumber(arrOne, arrTwo){
+    let takeElement = arrTwo[0];
+    let removeEl = arrTwo[1];
+    let searchEl = arrTwo[2]; 
+
+    let numbers = arrOne.slice(0,takeElement);
+    numbers.splice(0, removeEl);
+   
+    
+    let count = numbers.filter((el) => el === searchEl);
+    // for (const el of seqance) {
+    //     if (el === searchEl) {
+    //         counter++;
+    //     }       
+    // }
+    console.log(`Number ${searchEl} occurs ${count.length} times.`);
+}
+
+// searchForANumber(
+//     [5, 2, 3, 4, 1, 6],
+//     [5, 2, 3])
+// console.log('-------');
+searchForANumber([7, 1, 5, 8, 2, 7],
+    [3, 1, 5])
+
