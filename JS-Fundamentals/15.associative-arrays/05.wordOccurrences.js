e99c5de74f200c26ba616a20e97829ec384f2f1e
yosif88 <yosif.karaboychev88@gmail.com>
2023-06-29 11:57:45 +0300

    wordOccurrences

---
+++ JS-Fundamentals/15.associative-arrays/05.wordOccurrences.js
@@ -0,0 +1,25 @@
+function wordOccurrences(data){
+    let words = new Map();
+
+    for (const text of data) {
+        
+        if (words.has(text)){
+            let currnetNum = words.get(text);
+            currnetNum++;
+            words.set(text,currnetNum)
+        }else{
+            words.set(text,1)
+        }
+    }
+   
+    let sortedMap = Array.from(words.entries()).sort((kvpA,kvpB) => kvpB[1]-(kvpA[1]));
+    for (const [key, value] of sortedMap) {
+        console.log(`${key} -> ${value} times`);
+    }
+
+}
+
+wordOccurrences(["Here", 
+"is", 
+"the", "first", "sentence", "Here", "is", "another", "sentence", "And", "finally", "the", "third", 
+"sentence"])
