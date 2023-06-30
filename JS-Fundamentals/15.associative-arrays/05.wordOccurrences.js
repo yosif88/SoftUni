2f1595dbd02a1bc1cdb179aec056d8e6d42a396a
yosif88 <yosif.karaboychev88@gmail.com>
2023-06-30 11:00:23 +0300

    modified

--- JS-Fundamentals/15.associative-arrays/05.wordOccurrences.js
+++ JS-Fundamentals/15.associative-arrays/05.wordOccurrences.js
@@ -4,15 +4,15 @@ function wordOccurrences(data){
     for (const text of data) {
         
         if (words.has(text)){
-            let currnetNum = words.get(text);
-            currnetNum++;
-            words.set(text,currnetNum)
+            let counter = words.get(text);
+            counter++;
+            words.set(text,counter)
         }else{
             words.set(text,1)
         }
     }
    
-    let sortedMap = Array.from(words.entries()).sort((kvpA,kvpB) => kvpB[1]-(kvpA[1]));
+    let sortedMap = Array.from(words.entries()).sort((kvpA,kvpB) => kvpB[1]-kvpA[1]);
     for (const [key, value] of sortedMap) {
         console.log(`${key} -> ${value} times`);
     }
