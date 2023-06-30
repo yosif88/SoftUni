5374e35b6692942454c5bc38299dbb539c375706
yosif88 <yosif.karaboychev88@gmail.com>
2023-06-30 11:02:24 +0300

    modified

--- JS-Fundamentals/15.associative-arrays/05.wordOccurrences.js
+++ JS-Fundamentals/15.associative-arrays/05.wordOccurrences.js
@@ -1,25 +1,29 @@
-function wordOccurrences(data){
+function wordOccurrences(data) {
     let words = new Map();
 
     for (const text of data) {
-        
-        if (words.has(text)){
+
+        if (words.has(text)) {
+
             let counter = words.get(text);
             counter++;
-            words.set(text,counter)
-        }else{
-            words.set(text,1)
+
+            words.set(text, counter)
+
+        } else {
+            words.set(text, 1)
         }
     }
+
+    let sortedMap = Array.from(words.entries()).sort((kvpA, kvpB) => kvpB[1] - kvpA[1]);
    
-    let sortedMap = Array.from(words.entries()).sort((kvpA,kvpB) => kvpB[1]-kvpA[1]);
     for (const [key, value] of sortedMap) {
         console.log(`${key} -> ${value} times`);
     }
 
 }
 
-wordOccurrences(["Here", 
-"is", 
-"the", "first", "sentence", "Here", "is", "another", "sentence", "And", "finally", "the", "third", 
-"sentence"])
+wordOccurrences(["Here",
+    "is",
+    "the", "first", "sentence", "Here", "is", "another", "sentence", "And", "finally", "the", "third",
+    "sentence"])
