43626f3ed2df8cb7afa3e1c2a618c26095317422
yosif88 <yosif.karaboychev88@gmail.com>
2023-07-01 11:42:05 +0300

    wordsTracker

---
+++ JS-Fundamentals/16.exercise-associative-arrays/01.wordsTracker.js
@@ -0,0 +1,66 @@
+// first version Map
+
+// function wordsTracker(input) {
+//     let map = new Map();
+
+//     let wordsCheck = input.shift().split(' ');
+
+//     wordsCheck.forEach(el => {
+//         map.set(el, 0);
+//     })
+
+//     for (const word of input) {
+//         if (map.has(word)){
+//             let times = map.get(word);
+//             times ++;
+//             map.set(word, times);
+//         }
+
+//     }
+
+//     let sorted = Array.from(map.entries())
+//     .sort((kvpA, kvpB) => kvpB[1] - kvpA[1]);
+
+//     for (const [key, times] of sorted){
+
+//         console.log(`${key} - ${times}`);
+
+//     }
+
+// }
+
+
+// second version  Associative array /object/
+
+function wordsTracker(input) {
+    let words = {};
+
+    let wordsCheck = input.shift().split(' ');
+
+    wordsCheck.forEach(el => {
+        words[el] = 0;
+    })
+
+    for (const word of input) {
+        if (words.hasOwnProperty(word)) {
+            words[word] += 1;
+        }
+    }
+
+    let sorted = Object.entries(words)
+    .sort((kvpA, kvpB) => kvpB[1] - kvpA[1]);
+
+    for (const [key, times] of sorted) {
+
+        console.log(`${key} - ${times}`);
+
+    }
+
+}
+
+
+wordsTracker([
+    'is the',
+    'first', 'sentence', 'Here', 'is',
+    'another', 'the', 'And', 'finally', 'the',
+    'the', 'sentence'])
