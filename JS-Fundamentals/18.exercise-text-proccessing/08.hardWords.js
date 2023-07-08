49e0a64e4806c400e0ac7dd497ea579487b2ca3b
yosif88 <yosif.karaboychev88@gmail.com>
2023-07-08 18:40:06 +0300

    hardWords

---
+++ JS-Fundamentals/18.exercise-text-proccessing/08.hardWords.js
@@ -0,0 +1,19 @@
+function hardWords(input) {
+    let text = input[0];
+    let words = input[1]
+    words.sort((a, b) => b.length - a.length)
+    
+    for (const word of words) {
+        let template = '_'.repeat(word.length);
+        
+        while (text.includes(template)){
+            text = text.replace(template,word);
+        }
+    }
+    console.log(text);
+
+}
+
+hardWords(['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.',
+    ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']]
+)
