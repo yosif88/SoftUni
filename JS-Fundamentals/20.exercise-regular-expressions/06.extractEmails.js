cce038503b38ec9fb237df08ffc359c37958d791
yosif88 <yosif.karaboychev88@gmail.com>
2023-07-16 16:32:05 +0300

    extractEmails

---
+++ JS-Fundamentals/20.exercise-regular-expressions/06.extractEmails.js
@@ -0,0 +1,15 @@
+function extractEmails(input){
+    let pattern = /(?<=\s)([a-zA-Z0-9]+[\.\-\_]?[a-zA-Z0-9]+)@([a-zA-Z]+[\-]?[a-zA-Z]+)(\.\w+\.?\w+)/g
+    let matches = input.match(pattern);
+
+    for (const line of matches) {
+        console.log(line);
+    }
+
+}
+
+extractEmails('Please contact us at: support@github.com.')
+console.log('=================');
+extractEmails('Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.');
+console.log('==========================');
+extractEmails('Many users @ SoftUni confuse email addresses. We @ Softuni.BG provide high-quality training @ home or @ class. –- steve.parker@softuni.de.')
