438beed71a8d95f0e2bdb136df518e1ebaebf5bb
yosif88 <yosif.karaboychev88@gmail.com>
2023-09-23 21:52:53 +0300

    show-more

---
+++ JS-Advanced/01.DOM-Introduction/04.Show-More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let anchorElement = document.getElementById('more');
+    let spanElement = document.querySelector('#text');
+    spanElement.style.display = 'inline';
+    anchorElement.style.display='none';
+}
