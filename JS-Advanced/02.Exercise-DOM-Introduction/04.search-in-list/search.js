ec51a6cb0f3a3c32ca46f820c5739a5196a4d251
yosif88 <yosif.karaboychev88@gmail.com>
2023-09-24 17:44:23 +0300

    search-in-list

---
+++ JS-Advanced/02.Exercise-DOM-Introduction/04.search-in-list/search.js
@@ -0,0 +1,20 @@
+function search() {
+   let towns = Array.from(document.querySelectorAll('#towns li'));
+   let searchTextElement = document.querySelector('#searchText').value;
+   let result = document.getElementById('result');
+   let count = 0;
+
+     towns.forEach(town => {
+         if(town.textContent.includes(searchTextElement) && searchTextElement !== ''){
+            town.style.fontWeight = 'bold';
+            town.style.textDecoration = 'underline';
+            count++;
+
+         }else{
+            town.style.fontWeight = 'normal';
+            town.style.textDecoration = 'none';
+         }
+
+     })
+   result.textContent = `${count} matches found`
+}
