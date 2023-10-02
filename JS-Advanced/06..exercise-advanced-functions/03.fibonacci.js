7443c22139203561998c14aeeaaa7480a69a46e0
yosif88 <yosif.karaboychev88@gmail.com>
2023-10-02 22:47:53 +0300

    fibonacci

---
+++ JS-Advanced/06..exercise-advanced-functions/03.fibonacci.js
@@ -0,0 +1,24 @@
+function getFibonator() {
+    let firstNum = 0;
+    let secondNum = 1;
+    let sum = 0;
+
+    return function () {
+        sum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = sum;
+        
+        return firstNum;
+    }
+}
+
+
+
+let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
