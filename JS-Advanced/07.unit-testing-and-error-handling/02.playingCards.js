f01fa4ad04a39ad59a9c4444d81e0d6a365c33aa
yosif88 <yosif.karaboychev88@gmail.com>
2023-10-05 16:21:57 +0300

    playing-cards

---
+++ JS-Advanced/07.unit-testing-and-error-handling/02.playingCards.js
@@ -0,0 +1,28 @@
+function playingCards(face, suit) {
+    let faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    
+    let suits = {
+
+        'S': '\u2660',
+        'H': '\u2665',
+        'D': '\u2666',
+        'C': '\u2663'
+    }
+
+    if (!faces.includes(face) || !suits[suit]) {
+        throw new Error('Error')
+    }
+
+    return {
+        face,
+        suit,
+        toString(){
+            return `${this.face}${suits[this.suit]}`
+        }
+    }
+
+}
+
+console.log((playingCards('A', 'S').toString()))
+console.log(playingCards('10', 'H').toString())
+playingCards('1', 'C')
