213c71bbdee4f9ce073a75a4f5aeec2f1a354852
yosif88 <yosif.karaboychev88@gmail.com>
2024-02-24 16:46:48 +0200

    invalid number

---
+++ GO-BASIC/05.Conditional-Statements-Advanced-Lab/10.invalid-number/invalid-number.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var number int
+	fmt.Scanln(&number)
+
+	var isValid bool = (number >= 100 && number <= 200) || number == 0
+
+	if !isValid {
+		fmt.Printf("invalid")
+	}
+
+}
