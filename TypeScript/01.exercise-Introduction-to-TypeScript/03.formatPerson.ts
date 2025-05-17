323d01746a3c10b652c421b662555902763df05b
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-05-17 17:57:56 +0300

    add formatPerson

---
+++ TypeScript/01.exercise-Introduction-to-TypeScript/03.formatPerson.ts
@@ -0,0 +1,6 @@
+function formatPerson(personInfo:[string, number]): string{
+    return `Hello, my name is ${personInfo[0]} and my age is ${personInfo[1]}`
+}
+
+console.log(formatPerson(['Ivan', 20]));
+
---
+++ TypeScript/01.exercise-Introduction-to-TypeScript/out/03.formatPerson.js
@@ -0,0 +1,7 @@
+"use strict";
+function formatPerson(personInfo) {
+    return `Hello, my name is ${personInfo[0]} and my age is ${personInfo[1]}`;
+}
+console.log(formatPerson(['Ivan', 20]));
+console.log(formatPerson([]));
+//# sourceMappingURL=03.formatPerson.js.map
