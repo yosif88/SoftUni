c1bd409746bd0cff40bf9f825860109da0358562
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-09-05 21:53:47 +0300

    Create projectsCreation.cpp

---
+++ C++Basic/First-Steps-In-Coding-Lab/FirstStepsInCoding/projectsCreation/projectsCreation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string name;
+	cin >> name;
+
+	int numberOfProjects;
+	cin >> numberOfProjects;
+
+	int hours = numberOfProjects * 3;
+
+	cout << "The architect " << name << " will need" << hours << " hours to complete " << numberOfProjects << " project / s.";
+
+}
