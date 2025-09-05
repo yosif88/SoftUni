ec32a514d9fe6979053854ca826fdb8a738e34d1
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-09-05 22:08:16 +0300

    Create yardGreening.cpp

---
+++ C++Basic/First-Steps-In-Coding-Lab/FirstStepsInCoding/yardGreening/yardGreening.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	double yard;
+	cin >> yard;
+
+	double cost = yard * 7.61;
+	
+	double discount = cost * 0.18;
+
+	double finalCost = cost - discount;
+	
+	cout << "The final price is: " << finalCost << "lv." << endl;
+	cout << "The discount is: " << discount << "lv.";
+	
+	return 0;
+
+
+}
