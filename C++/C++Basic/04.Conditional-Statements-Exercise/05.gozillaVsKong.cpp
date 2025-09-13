ec6283be794dac147ce5bbe80b63bcd4b5bfee4f
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-09-13 17:35:16 +0300

    Create 05.gozillaVsKong.cpp

---
+++ C++/C++Basic/04.Conditional-Statements-Exercise/05.gozillaVsKong.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double budget, priceClothing;
+    int numberOfExtra;
+
+    cin >> budget;
+    cin >> numberOfExtra;
+    cin >> priceClothing;
+
+    double decorCost = budget * 0.10;
+    double sumOfClothing = numberOfExtra * priceClothing;
+
+    if (numberOfExtra > 150)
+    {
+        sumOfClothing *= 0.90;
+    }
+
+    double totalCost = sumOfClothing + decorCost;
+
+   
+    cout.setf(ios::fixed);
+    cout.precision(2);
+
+    if (totalCost <= budget)
+    {
+         double leftMoney = abs(budget - totalCost);
+
+        cout << "Action!" << endl;
+        cout << "Wingard starts filming with " << leftMoney << " leva left." << endl;
+    }
+    else
+    {
+        cout << "Not enough money!" << endl;
+        double moneyNeeded = abs(budget - totalCost);
+
+        cout << "Wingard needs " << moneyNeeded << " leva more.";
+    }
+}
