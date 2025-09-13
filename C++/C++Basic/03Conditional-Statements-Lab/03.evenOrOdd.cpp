41a0ae01db834c1720a2147473165c4cd60f9be1
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-09-13 15:26:19 +0300

    Create 03.evenOrOdd.cpp

---
+++ C++/C++Basic/03Conditional-Statements-Lab/03.evenOrOdd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cin >> number;
+    if(number % 2 == 0){
+        cout << "even" << endl;
+    }
+    else{
+        cout << "odd" << endl;
+    }
+    
+    return 0;
+}
