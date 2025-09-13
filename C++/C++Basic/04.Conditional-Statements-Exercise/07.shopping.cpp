49244f2ec26575bdf61ba3289599dba01398ddcf
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-09-13 18:10:26 +0300

    Create 07.shopping.cpp

---
+++ C++/C++Basic/04.Conditional-Statements-Exercise/07.shopping.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double budget;
+    int countVga, countCpu, countRam;
+
+    cin >> budget >> countVga >> countCpu >> countRam;
+
+    double priceVga = countVga * 250;
+    double priceCpu = priceVga * 0.35 * countCpu;
+    double priceRam = priceVga * 0.10 * countRam;
+
+    double totalPrice = priceCpu + priceVga + priceRam;
+
+    if (countVga > countCpu)
+    {
+        totalPrice *= 0.85;
+    }
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+
+    if (budget >= totalPrice)
+    {
+        double leftMoney = budget - totalPrice;
+
+        cout << "You have " << leftMoney << " leva left!" << endl;
+    }
+    else
+    {
+        double moneyNeeded = abs(budget - totalPrice);
+
+        cout <<"Not enough money! You need " << moneyNeeded <<" leva more!" << endl;
+    }
+
+    return 0;
+}
