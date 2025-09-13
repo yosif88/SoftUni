5d67fe1bf526106718e6d486d7fb1cd09f0139d0
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-09-13 17:14:17 +0300

    Create 04.toyShop.cpp

---
+++ C++/C++Basic/04.Conditional-Statements-Exercise/04.toyShop.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double priceExcursion;
+    int countPuzzles, countDolls, countBears, countMinions, countTrucks;
+    cin >> priceExcursion;
+    cin >> countPuzzles >> countDolls >> countBears >> countMinions >> countTrucks;
+
+    double sumOfToys = (countPuzzles * 2.60) 
+    + (countDolls * 3) 
+    + (countBears * 4.10)
+    + (countMinions * 8.20)
+    + (countTrucks * 2.0);
+
+    int totalCountToys = countPuzzles + countDolls + countBears + countMinions + countTrucks;
+
+    if (totalCountToys >= 50) {
+        sumOfToys *= 0.75;
+    }
+
+    sumOfToys *= 0.90;
+    
+    double leftMoney = sumOfToys - priceExcursion;
+    
+    cout.setf(ios::fixed);
+    cout.precision(2);
+
+    if (sumOfToys >= priceExcursion){
+        cout << "Yes! " << leftMoney << " lv left." << endl;
+    }
+    else{
+        double neededMoney = priceExcursion - sumOfToys;
+
+        cout << "Not enough money! " << neededMoney << " lv needed." << endl;
+    }
+   
+    return 0;
+
+
+}
