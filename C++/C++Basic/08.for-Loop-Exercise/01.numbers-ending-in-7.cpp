69d8d41ddba151349f271e10e56ed1b276262e40
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-09-25 19:25:04 +0300

    Create 01.numbers-ending-in-7.cpp

---
+++ C++/C++Basic/08.for-Loop-Exercise/01.numbers-ending-in-7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //ver 1
+
+    // for (int i = 7; i < 1000; i+= 10)
+    // {
+    //     if (i % 10 == 7){
+    //         cout << i << endl;
+    //     }
+    // }
+
+    //ver 2
+    for (int number = 7; number < 1000; number+= 10)
+    {
+        cout << number << endl ;
+    }
+    
+     return 0;
+
+    
+}
