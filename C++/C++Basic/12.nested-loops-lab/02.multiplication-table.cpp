0c4e26e70f7f1c55058e3fb740b03b1c0ebeb033
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-10-11 20:08:23 +0300

    Create 02.multiplication-table.cpp

---
+++ C++/C++Basic/12.nested-loops-lab/02.multiplication-table.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    for (int numberOne = 1; numberOne <= 10; numberOne++)
+    {
+       for (int numberTwo = 1; numberTwo <= 10; numberTwo++)
+       {
+         cout << numberOne << " * " << numberTwo << " = " << numberOne * numberTwo << endl;
+       }
+       
+    }
+    
+}
