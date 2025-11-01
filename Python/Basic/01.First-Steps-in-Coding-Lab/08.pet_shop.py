31602f875c30d89a03f8cd3d1c11bc4c0e5f6cab
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-11-01 16:18:10 +0200

    Create 08.pet_shop.py

---
+++ Python/Basic/01.First-Steps-in-Coding-Lab/08.pet_shop.py
@@ -0,0 +1,6 @@
+number_of_packages_dog_food = int(input())
+number_of_packages_cat_food = int(input())
+
+total_sum = (number_of_packages_dog_food * 2.50) + (number_of_packages_cat_food * 4)
+
+print(f"{total_sum} lv.")
