79f9fe0ad7d68f6fd25b8546069c2eb67859b2a7
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-11-01 20:21:51 +0200

    Create 08.basketball_equipment.py

---
+++ Python/Basic/02.First-Steps-in-Coding-Exercise/08.basketball_equipment.py
@@ -0,0 +1,10 @@
+price = int(input())
+
+basketball_shoes = price * 0.60
+basketball_outfit = basketball_shoes * 0.80
+basketball_ball = basketball_outfit / 4
+basketball_accessories = basketball_ball / 5
+
+total_sum = price + basketball_shoes + basketball_outfit + basketball_ball + basketball_accessories
+
+print(total_sum)
