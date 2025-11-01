b64dde73bd7201a8a97535619494a674260ce266
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-11-01 16:14:36 +0200

    Create 07.projects_creation.py

---
+++ Python/Basic/01.First-Steps-in-Coding-Lab/07.projects_creation.py
@@ -0,0 +1,6 @@
+name = input()
+number_of_projects = int(input())
+
+needed_hours = number_of_projects * 3
+
+print(f"The architect {name} will need {needed_hours} hours to complete {number_of_projects} project/s.")
