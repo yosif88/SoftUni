ac892cdac87badb947d7c26c441cfcc937c461eb
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-11-05 19:38:59 +0200

    Create 2.passed-or-failed.cpp

---
+++ C++/Fundamentals/01.basic-syntax/.vscode/2.passed-or-failed.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float grade;
+    cin >> grade;
+    
+    cout << (grade >= 3 ? "Passed!":"Failed!");
+   
+    // if (grade >= 3)
+    // {
+    //     cout << "Passed!" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Failed!" << endl;
+    // }
+
+    return 0;
+}
