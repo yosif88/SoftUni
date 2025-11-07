cc7b6f23907892793dd9e4e5bc02e176cffc7052
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-11-07 19:22:28 +0200

    Create 04.numbers-1-to-N.cpp

---
+++ C++/Fundamentals/02.basic-syntax-exercise/04.numbers-1-to-N.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int num = 1; num <= n; num++)
+    {
+        cout << num << ' ';
+    }
+    
+    return 0;
+}
