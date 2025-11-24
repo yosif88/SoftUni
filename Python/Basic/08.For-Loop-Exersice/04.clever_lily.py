e414b1c7a741985d316637a838a0752aac8fbc4c
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-11-24 21:07:29 +0200

    Create 04.clever_lily.py

---
+++ Python/Basic/08.For-Loop-Exersice/04.clever_lily.py
@@ -0,0 +1,24 @@
+ages = int(input())
+washing_machine_price = float(input())
+price_toys = int(input())
+
+money_spend = 10
+total_sum = 0
+count_toys = 0
+
+
+for age in range(1, ages+1):
+
+    if (age % 2 == 0):
+        total_sum  += money_spend
+        money_spend += 10
+        total_sum -= 1
+    else:
+        count_toys += 1
+
+total_sum += count_toys * price_toys
+
+if total_sum >= washing_machine_price:
+    print(f"Yes! {abs(total_sum - washing_machine_price):.2f}")
+else:
+    print(f"No! {abs(total_sum - washing_machine_price):.2f}")
