f308eca1c0f946a45b780a8c65953b02d8a8bd56
Yosif Karaboychev <87412264+yosif88@users.noreply.github.com>
2025-11-24 21:09:18 +0200

    04.clever_lily

--- Python/Basic/08.For-Loop-Exersice/04.clever_lily.py
+++ Python/Basic/08.For-Loop-Exersice/04.clever_lily.py
@@ -2,7 +2,7 @@ ages = int(input())
 washing_machine_price = float(input())
 price_toys = int(input())
 
-money_spend = 10
+money_save = 10
 total_sum = 0
 count_toys = 0
 
@@ -10,8 +10,8 @@ count_toys = 0
 for age in range(1, ages+1):
 
     if (age % 2 == 0):
-        total_sum  += money_spend
-        money_spend += 10
+        total_sum  += money_save
+        money_save += 10
         total_sum -= 1
     else:
         count_toys += 1
